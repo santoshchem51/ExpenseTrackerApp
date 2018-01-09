3ee096595db63873bc55c6af0e409f5d9634407a
santoshchem51 <net.santosh51@gmail.com>
2018-01-09 14:29:29 -0600

    moved to scss and added side nav bar

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -2,8 +2,14 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'nav-bar',
-    templateUrl: './navbar.component.html'
+    templateUrl: './navbar.component.html',
+    styleUrls:['./navbar.component.scss']
 })
 export class NavBarComponent {
     navdefault : string = "default nav bar";
+    showDropDown: boolean = false;
+
+    toogleDropDown() : void {       
+        this.showDropDown = !this.showDropDown;        
+    }
 }
